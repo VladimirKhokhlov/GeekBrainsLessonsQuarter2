775aa5f6b782a025b431abf33811f2e8d124edbc
Vladimir Khokhlov <vladimirkhokhlov90@gmail.com>
2021-02-26 00:13:44 +0400

    Готовое дз) Окончательный вариант!

--- src/lesson3/Phonebook.java
+++ src/lesson3/Phonebook.java
@@ -1,35 +1,31 @@
 package lesson3;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class Phonebook {
-    static List<String[]> phoneBook = new ArrayList<>();
+    static TreeMap<String, HashSet<String>> phoneBook = new TreeMap<>();
 
     public static void main(String[] args) {
         add("Хохлов", "+1");
         add("Хохлов", "+22");
         add("Хохлов", "+333");
+
         add("Петров", "+9999");
         add("Петров", "+8888");
+
         get("Петров");
         get("Хохлов");
     }
 
     public static void add(String lastName, String number) {
-        for (String[] contact : phoneBook) {
-            if (lastName.equals(contact[0])){
-                phoneBook.lastIndexOf(number);
-            }
+        if (phoneBook.containsKey(lastName)) {
+            phoneBook.put(lastName, new HashSet<>());
         }
-        phoneBook.add(new String[] {lastName,number});
+        phoneBook.get(lastName).add(number);
     }
 
     public static void get(String lastName) {
-        for (String[] contact : phoneBook) {
-            if (lastName.equals(contact[0]))
-            System.out.println(Arrays.toString(contact));
-        }
+        if (phoneBook.containsKey(lastName))
+            System.out.println(lastName + phoneBook.get(lastName));
     }
 }
