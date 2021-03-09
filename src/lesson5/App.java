1c2a830c0c4e75e3b1fe2ce2dae1423a11077bcf
Vladimir Khokhlov <vladimirkhokhlov90@gmail.com>
2021-03-09 13:37:26 +0400

    Последний вариант

--- src/lesson5/App.java
+++ src/lesson5/App.java
@@ -5,7 +5,6 @@ public class App {
     static final int size = 10000000;
     static final int h = size / 2;
     static float[] arr = new float[size];
-    static long a = System.currentTimeMillis();
 
     public static void main(String[] args) {
         methodOne();
@@ -16,7 +15,7 @@ public class App {
         for (int i = 0; i < size; i++) {
             arr[i] = 1;
         }
-        System.currentTimeMillis();
+        long a = System.currentTimeMillis();
         for (int i = 0; i < size; i++) {
             arr[i] = (float) (arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
         }
@@ -29,24 +28,24 @@ public class App {
         float[] a2 = new float[h];
         new Thread(() -> {
             System.arraycopy(arr, 0, a1, 0, h);
-            System.currentTimeMillis();
+            long a = System.currentTimeMillis();
             for (int i = 0; i < a1.length; i++) {
                 arr[i] = 1;
                 arr[i] = (float) (arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
             }
-            System.currentTimeMillis();
+//            System.currentTimeMillis();
             System.arraycopy(a1, 0, arr, 0, h);
             System.currentTimeMillis();
             System.out.println(System.currentTimeMillis() - a);
         }).start();
         new Thread(() -> {
             System.arraycopy(arr, h, a2, 0, h);
-            System.currentTimeMillis();
+            long a = System.currentTimeMillis();
             for (int i = 0; i < a2.length; i++) {
                 arr[i] = 1;
                 arr[i] = (float) (arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
             }
-            System.currentTimeMillis();
+//            System.currentTimeMillis();
             System.arraycopy(a2, 0, arr, h, h);
             System.currentTimeMillis();
             System.out.println(System.currentTimeMillis() - a);
