ac1d9f9b21595a7c708aa4c697116a7c97ab10c8
Vladimir Khokhlov <vladimirkhokhlov90@gmail.com>
2021-03-13 11:38:11 +0400

    Вроде всё сделал, но, собака, не работает так как надо!

--- src/lesson6/ConsoleClient.java
+++ src/lesson6/ConsoleClient.java
@@ -7,5 +7,37 @@ import java.net.Socket;
 import java.util.Scanner;
 
 public class ConsoleClient {
+    private static final Scanner scanner = new Scanner(System.in);
 
+    public static void main(String[] args) {
+        try (Socket socket = new Socket("localhost", 65501)) {
+            System.out.println("Connected");
+
+            DataInputStream in = new DataInputStream(socket.getInputStream());
+            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+
+            Thread write = new Thread(() -> {
+                while (true) {
+                    try {
+                    String msg = scanner.nextLine();
+                        out.writeUTF("Client send: " + msg);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+            write.start();
+            write.setDaemon(true);
+                while (true) {
+                    try {
+                        String massage = in.readUTF();
+                        System.out.println(massage);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
